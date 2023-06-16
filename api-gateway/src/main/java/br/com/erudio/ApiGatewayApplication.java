ff4fde5d909a90103b8e0bff05de6c92047b3657
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-06-16 12:28:55 -0300

    wip: load balancing with eureka, feign e spring cloud loadbalancer

--- api-gateway/src/main/java/br/com/erudio/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/erudio/ApiGatewayApplication.java
@@ -2,14 +2,12 @@ package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-public class BookServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
---
+++ book-service/src/main/java/br/com/erudio/model/Book.java
@@ -0,0 +1,120 @@
+package br.com.erudio.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity(name = "book")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Book implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "author", nullable = false, length = 180)
+    private String author;
+
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date lauchDate;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false, length = 250)
+    private String title;
+
+    @Transient
+    private String currency;
+
+    @Transient
+    private String environment;
+
+    public Book() {}
+
+    public Book(Long id, String author, String title, Date lauchDate, Double price, String currency, String environment) {
+        this.id = id;
+        this.author = author;
+        this.lauchDate = lauchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLauchDate() {
+        return lauchDate;
+    }
+
+    public void setLauchDate(Date lauchDate) {
+        this.lauchDate = lauchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(author, book.author) && Objects.equals(lauchDate, book.lauchDate) && Objects.equals(price, book.price) && Objects.equals(title, book.title) && Objects.equals(currency, book.currency) && Objects.equals(environment, book.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, lauchDate, price, title, currency, environment);
+    }
+}
