d08899488a0fc7801cdd0e74f6c28d5c79096989
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-06-21 01:17:22 -0300

    work in progress

--- book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -5,6 +5,8 @@ import br.com.erudio.model.Book;
 import br.com.erudio.proxy.CambioProxy;
 import br.com.erudio.repository.BookRepository;
 import br.com.erudio.response.Cambio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -26,6 +29,8 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
             @PathVariable("id") Long id,
